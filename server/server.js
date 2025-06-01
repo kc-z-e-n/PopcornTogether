b93abbffd4a160f0b0b486fc88d86efa0fb3d8c5
gtellurium <e1354845@u.nus.edu>
2025-06-01 22:37:31 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,3 +25,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api', authRoutes);
