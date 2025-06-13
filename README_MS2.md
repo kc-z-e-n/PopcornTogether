03597decf4b1215ed129fd684ce9273bc2a1ec16
kc-z-e-n <kkxchoo@email.com>
2025-06-13 16:42:26 +0800

    Add movieRoutes.js filter.js component

---
+++ README_MS2.md
@@ -0,0 +1,21 @@
+# Orbital 25 Milestone 2
+## Team name
+PopcornTogether
+
+## Proposed Level of Achievement
+Apollo 11
+
+## Objectives
+The objectives for milestone 2 are as listed below:
+- Set up frontend
+- Set up backend
+- Create basic features
+    - Account registration
+    - Login
+    - Profile
+    - Movie Search
+
+## Comleted features
+- Webapp: Hosted locally on localhost
+- Account registration: User data stored using  mongoDB integration
+- Account login: Implemented using Express
---
+++ server/routes/movieRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+require('dotenv').config();
+
+router.get('/search', async (req, res) => {
+    const { genre, year, language} = req.query;
+  
+    try {
+      const tmdb = await axios.get('https://api.themoviedb.org/3/discover/movie', {
+        params : {
+            api_key: process.env. TMDB_API_KEY,
+            genres: genre,
+            release_year: year,
+            language: language || 'en_US',
+            sort_by: 'popularity.desc',
+            page: 1,
+        },
+      });
+
+      const top10 = tmdb.data.results.slice(0, 10);
+      res.json(top10);
+
+    } catch (err) {
+      res.status(500).json({ error: 'TMDB Request failed' });
+    }
+  });
+
+  module.exports = router;
