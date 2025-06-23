3a8d9fb80040836b8c8c5ae5f46ec0dd2e8e0a6d
kc-z-e-n <kkxchoo@email.com>
2025-06-23 18:31:08 +0800

    Add Logout route

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from  'react';
+import React, {useEffect, useState} from  'react';
 import { Link, useNavigate} from 'react-router-dom';
 import './Header.css';
 import axios from 'axios';
@@ -6,11 +6,28 @@ import axios from 'axios';
 const Header = ({onSearchBarFocus, onSearch}) => {
     const navigate = useNavigate();
     const [query, setQuery] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const checkLogin = async () => {
+            try {
+                const res = await axios.get('http://localhost:5050/api/me', {
+                    withCredentials:true
+                });
+                if (res.status === 200) {
+                    setIsLoggedIn(true);
+                }
+            } catch (err) {
+                setIsLoggedIn(false)
+            }
+        };
+        checkLogin();
+    }, []);
 
     const handleProtectedRoute = async (path) => {
         try {
             const res = await axios.get('http://localhost:5050/api/me', {withCredentials: true}) ;
-            if (res.status==200) {
+            if (res.status === 200) {
                 navigate(path);
             }
         } catch(err) {
@@ -19,14 +36,31 @@ const Header = ({onSearchBarFocus, onSearch}) => {
         }
     };
 
+    const handleLogout = async () => {
+        try {
+            await axios.post('http://localhost:5050/api/logout', {}, {
+                withCredentials: true
+            });
+            setIsLoggedIn(false);
+            navigate('/auth');
+        } catch (err) {
+            console.error('logout error: ', err);
+            alert('Logout failed');
+        }
+    }
+
     return (
         <header className="header">
             <div className='header-left'>
-                <img src='PT_logo.jpg' alr='PopocornTogether' className='logo'/>
+                <img src='PT_logo.jpg' alt='PopocornTogether' className='logo'/>
                 <Link to="/" className='brand'>PopcornTogether</Link>
             </div>
             <nav className='header-nav'>
-                <Link to="/auth" className='nav-link'>LOGIN</Link>
+                {!isLoggedIn ? (
+                    <Link to="/auth" className='nav-link'>LOGIN</Link>
+                ) : (
+                    <span className='nav-link' onClick={handleLogout}>LOGOUT</span>
+                )}
                 <span className='nav-link' onClick={() => handleProtectedRoute('/profile')}>MY PROFILE</span>
                 <span className='nav-link' onClick={() => handleProtectedRoute('/lists')}>MY LISTS</span>
                 <span className='nav-link' onClick={() => handleProtectedRoute('/friends')}>MY FRIENDS</span>
